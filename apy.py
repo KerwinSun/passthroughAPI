e5a4785610e032ef3200299f98fd405b892deb0d
kerwin10010@gmail.com <kerwin10010@gmail.com>
2022-01-10 15:27:06 -0800

    second commit

--- apy.py
+++ apy.py
@@ -1,11 +1,29 @@
 import flask
+import subprocess
+from flask import request
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>API home placeholder</h1>"
+
+@app.route('/passthrough/api/v1/executeCommand', methods=['GET'])
+def api_cmd():
+    if 'cmd' in request.args:
+    	cmd = request.args['cmd']
+    else:
+    	return "error"
+
+    out = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, text=True)
+    return "<div> command output: " + out.stdout + "</div>"
+    
+@app.route('/passthrough/api/v1/health', methods=['GET'])
+def api_health():
+    out = subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE)
+    subprocess_return = out.stdout.read()
+    return "command output:" + str(subprocess_return)
 
 app.run()
