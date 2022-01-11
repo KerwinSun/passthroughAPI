33904e17c24958a6a37e54ec624c9f59d49187b6
kerwin10010@gmail.com <kerwin10010@gmail.com>
2022-01-10 17:10:09 -0800

    change endpoints

--- app.py
+++ app.py
@@ -3,23 +3,31 @@ import subprocess
 from flask import request
 
 app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+p = False
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>API home placeholder</h1>"
 
 @app.route('/passthrough/api/v1/executeCommand', methods=['GET'])
-def api_cmd():
+def api_executeCommand():
+    global p
     if 'cmd' in request.args:
     	cmd = request.args['cmd']
     else:
-    	return "error"
+    	return "error no command found"
 
-    out = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, text=True)
-    return "<div> command output: " + out.stdout + "</div>"
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr = subprocess.STDOUT)
+    return "command: " + cmd + " written succesfully"
+
+@app.route('/passthrough/api/v1/getCommandOutput', methods=['GET'])
+def api_getCommandOutput():
+    global p
+    return "<div> command output: " + p.stdout.readline().decode() + "</div>"
     
 @app.route('/passthrough/api/v1/health', methods=['GET'])
 def api_health():
-    out = subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE)
-    subprocess_return = out.stdout.read()
-    return "command output:" + str(subprocess_return)
+    p = subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    return "command output:" + out.decode()
